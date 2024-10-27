3d1c3df865b9aa801118058affa8aea1c657f478
amercado <arnold.mercado@hotmail.com>
2024-10-27 15:47:13 +0800

    Tailwind CSS Setup and Theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,38 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#FF7000",
+          100: "#FF7000",
+          800: "#FFF1E6",
+        },
+        light: {
+          DEFAULT: "#FFFFFF",
+          400: "#858EAD",
+          500: "#7B8EC8",
+          700: "#DCE3F1",
+          800: "#F4F6F8",
+          850: "#FDFDFD",
+          900: "#FFFFFF",
+        },
+        dark: {
+          DEFAULT: "#000000",
+          500: "#3F4354",
+          400: "#212734",
+          300: "#151821",
+          200: "#151821",
+          100: "#0000",
+       
+        },
       },
+      spacing: {},
+      baxShadow: {
+        
+      },
+      screens: {
+        "xs":"420px",
+        
+      }
     },
   },
   plugins: [],
