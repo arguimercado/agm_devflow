91d0e5ded9beb7a17d789085f87e4212cb92608e
amercado <arnold.mercado@hotmail.com>
2024-11-24 16:36:01 +0800

    Implement dynamic account route

---
+++ src/app/api/accounts/provider/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/https-errors";
+import { AccountSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const account = await User.findOne({ providerAccountId });
+    if (!account) {
+      throw new NotFoundError("Account");
+    }
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
